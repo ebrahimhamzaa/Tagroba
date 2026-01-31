991a4bcac760a3735373c385b520d91618042c85
ebrahimhamzaa <ebrahimhamza152@gmail.com>
2026-01-31 16:17:20 +0200

    footer updated

---
+++ src/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div style={{
+        backgroundColor :"gray",
+        padding: "10px"
+    }}>
+        footer
+    </div>
+  )
+}
